dd0e32c335f01469d646726fcdb868200ab04627
grehochka82 <lobodina0607@mail.ru>
2026-01-23 09:12:36 +0300

    Terminal file operations

---
+++ backend/program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Меня зовут Лободина В.А");
+Console.WriteLine(DateTime.Now);
